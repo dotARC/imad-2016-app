971c581c8071d0ab1f239d0f6db17d8c81473bfa
dotARC <mukeshbarca@gmail.com>
2016-09-30 03:26:39 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/LOGO1.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'LOGO1.png'));
 });
 
