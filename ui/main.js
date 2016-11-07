80ec45dd4e55ebe0933958f058dea10632c6c4a5
dotARC <mukeshbarca@gmail.com>
2016-11-07 12:28:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,6 +37,47 @@ window.onclick = function(event) {
   }
 }
 
+var counter=0;
+   
+    var button = document.getElementById('submit_btn');
+    button.onclick = function(){
+    	 //counter=counter+1; this was just client side without visiting server
+    	 
+    	 //this is requesting server for information
+    	 //Making a request to counter endpoint
+    	 //Create request object
+    	 var request = new XMLHttpRequest();
+         var nameInput = document.getElementById('name');
+         var name=nameInput.value;
+         console.log('name is: ',name);
+    	 //Capture the response and store it in a variable
+    	 request.onreadystatechange = function(){
+    	 	if(request.readyState === XMLHttpRequest.DONE){
+    	 		//take some action
+    	 		if(request.status ===200){
+    	 			var names=request.responseText;//this gets a string
+                    console.log('names1 is: ',names);
+                    names = JSON.parse(names); //converting string to array
+                    console.log('names is: ',names);
+                    var list='';
+                    for(var i=0;i<names.length;i++){
+                    list=list +'<li>'+names[i]+"</li>";
+                    console.log('list is: ',list);
+                    }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+    	 		}
+    	 	}
+    	 
+    	 	
+    	 };//button onclick function ends
+
+    	 //Make the request
+    	 request.open('GET','http://dotarc.imad.hasura-app.io/submit-name?name='+name,true);
+    	 request.send(null);
+    	
+    }; //button onclick function ends
+    
 //return comment typed in comment box
 var commentBtn = document.getElementById('comment_btn');
 commentBtn.onclick = function (){
@@ -71,3 +112,6 @@ commentBtn.onclick = function (){
    request.send(null);
 
 };//button onclick function ends
+
+
+  
