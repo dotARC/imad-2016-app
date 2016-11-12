1923264c30db526f377bc463577717b79f10ce31
dotARC <mukeshbarca@gmail.com>
2016-11-12 14:29:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,85 @@ var pool= require('pg').pool;
 var app = express();
 app.use(morgan('combined'));
 
+var config = {
+    user:'dotarc',
+    database: 'dotarc',
+    host: 'db.hasura.imad.io',
+    port: '5432',
+    password: password.env.DB_PASSWORD
+};
+
+function createtemplate(data) {
+    var title=data.title;
+    var page=data.page;
+    var heading= data.heading;
+    var date= data.date;
+    var content= data.content;
+    
+    var htmltemplate =`
+    <html>
+        <head>
+            <link rel="icon" href="/ui/LOGO1.ico" >
+            <title>
+               ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" >
+             <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <BODY>
+           
+                <div class="container">
+                <h1>${page}</h1>
+              
+                
+                <div align="right"> 
+                <a href="/home"><button  class="button1">HOME</button></a>
+                 <a href="/Profile"><button  class="button2">PROFILE</button></a>
+                 </div>
+                 <hr>
+            
+            <h1>
+                ${heading}
+            </h1>
+            <div>${date.toDateString()}</div>
+            <div>
+                $(content)
+            </div>
+            </div>
+            <h4>Comments</h4>
+    
+            <div id="comments">
+             <center>Loading Comments..</center>
+            </div>
+            <div id="comment_form" ></div>
+	
+            </div>
+             <script type="text/javascript" src="/ui/article.js" ></script>
+        </BODY>
+    </html>`;
+    return htmltemplate;
+}
+
+var pool = new Pool(config);
+
+app.get('/article/:articleName', function (req, res) {
+
+	var articleName = req.params.articleName;
+
+	pool.query('SELECT * FROM article WHERE title=$1',[articleName],function(err,result){
+		if(err){
+           res.status(500).send(err.toString());
+       }else{
+           if(result.rows.length === 0){
+           	res.status(404).send('Article Not Found.');
+           }else{
+                var articleData = result.rows[0];
+           	res.send(createTemplate(articleData));
+           }
+       }
+	});
+});
+
 
 
 
