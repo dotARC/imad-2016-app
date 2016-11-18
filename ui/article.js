f770eec650dc8b374ea6798f4285b72df50fd52b
dotARC <mukeshbarca@gmail.com>
2016-11-18 23:29:20 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,97 +1,35 @@
-function loadCommentForm () {
-    var commentFormHtml = `
-        <h5>Submit a comment</h5>
-        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
-        <br/>
-        <input type="submit" id="submit" value="Submit" />
-        <br/>
-        `;
-    document.getElementById('comment_form').innerHTML = commentFormHtml;
-    
-    // Submit username/password to login
-    var submit = document.getElementById('submit');
-    submit.onclick = function () {
-        // Create a request object
-        var request = new XMLHttpRequest();
-        
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-                // Take some action
-                if (request.status === 200) {
-                    // clear the form & reload all the comments
-                    document.getElementById('comment_text').value = '';
-                    loadComments();    
-                } else {
-                    alert('Error! Could not submit comment');
-                }
-                submit.value = 'Submit';
-          }
-        };
-        
-        // Make the request
-        var comment = document.getElementById('comment_text').value;
-        request.open('POST', '/submit-comment/' + currentArticleTitle, true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({comment: comment}));  
-        submit.value = 'Submitting...';
-        
-    };
-}
-
-function loadLogin () {
-    // Check if the user is already logged in
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                loadCommentForm(this.responseText);
+// Submit a comment
+var commentbtn = document.getElementById('comment-button');
+if (commentbtn !== undefined) {
+commentbtn.onclick = function(){
+    //Make request to server to send the comments
+    var request1 = new XMLHttpRequest();
+    request1.onreadystatechange = function (){
+        if(request1.readyState===XMLHttpRequest.DONE){
+            if(request1.status===200){
+    //Store the comments and display on the page
+    var comments = request1.responseText;
+        comments = JSON.parse(comments);
+        list_comment = '';
+        for(var j = 0; j<comments.length;j++){
+            list_comment += '<li>'+comments[j]+'</li>';
+        }
+            var ul1 = document.getElementById('commentlist');
+            ul1.innerHTML = list_comment;
             }
         }
     };
     
-    request.open('GET', '/check-login', true);
+   
+    //Make request to capture the comment
+    var commentInput = document.getElementById('comment');
+    var comment = commentInput.value;
+     request.open('GET','http://dotarc.imad.hasura-app.io/submit-comment?comment='+comment,true);
+   //for local machine 
+
+   request.open('GET', window.location.protocol+'//'+window.location.host+'/submit-comment?comment='+comment, true);
+   //for local machine 
+   //request.open('GET',document.URL+'counter',true);
     request.send(null);
+};
 }
-
-function escapeHTML (text)
-{
-    var $text = document.createTextNode(text);
-    var $div = document.createElement('div');
-    $div.appendChild($text);
-    return $div.innerHTML;
-}
-
-function loadComments () {
-        // Check if the user is already logged in
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            var comments = document.getElementById('comments');
-            if (request.status === 200) {
-                var content = '';
-                var commentsData = JSON.parse(this.responseText);
-                for (var i=0; i< commentsData.length; i++) {
-                    var time = new Date(commentsData[i].timestamp);
-                    content += `<div class="comment">
-                        <p>${escapeHTML(commentsData[i].comment)}</p>
-                        <div class="commenter">
-                            ${commentsData[i].username} - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()} 
-                        </div>
-                    </div>`;
-                }
-                comments.innerHTML = content;
-            } else {
-                comments.innerHTML('Oops! Could not load comments!');
-            }
-        }
-    };
-    
-    request.open('GET', '/get-comments/' + currentArticleTitle, true);
-    request.send(null);
-}
-
-
-// The first thing to do is to check if the user is logged in!
-loadLogin();
-loadComments();
