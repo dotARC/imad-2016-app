28395e6383cf85b6cb5a788e64e02228b49b5dfb
dotARC <mukeshbarca@gmail.com>
2016-11-18 22:14:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -236,7 +236,7 @@ app.get('/article/:articleName', function (req, res) {
         if (result.rows.length === 0) {
             res.status(404).send('Article not found');
         } else {
-            var articleData = result.rows[0];
+            var article = result.rows[0];
             res.send(createTemplate(article));
         }
     }
