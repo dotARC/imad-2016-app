c27ddded799f9a56b8fd739a36513eeee1276161
dotARC <mukeshbarca@gmail.com>
2016-11-27 00:07:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,32 @@
 
+setTimeout(glow,50);
+function glow()
+{   
+    if(i<1){
+    document.getElementById("logo").style.opacity=i;
+    i=i+0.1;
+    setTimeout(glow,100);
+    
+    }
+    else
+    {
+        setTimeout(glowagain,50);
+    }
+}
+function glowagain()
+{
+    if(i>0)
+    {
+      document.getElementById("logo").style.opacity=i;
+    i=i-0.1;
+    setTimeout(glowagain,100);
+    
+    }
+    else
+    {
+        setTimeout(glow,100);
+    }
+}
 
 
 /* When the user clicks on the button, 
