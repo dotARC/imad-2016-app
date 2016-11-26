d324eb0f0512254439ca0a3d0fc729e6e3a7f200
dotARC <mukeshbarca@gmail.com>
2016-11-26 12:19:11 +0530

    [imad-console] Updates ui/login.js

--- ui/login.js
+++ ui/login.js
@@ -1,105 +1,131 @@
 
-console.log('Loaded!');
-
-
-var submit = document.getElementById('submit-btn');
-var register = document.getElementById('register-btn');
-
-
-submit.onclick = function(){
+function loadLoginForm () {
+    var loginHtml = `
+        <h3>Login/Register </h3>
+        <input type="text" id="username" placeholder="username" />
+        <input type="password" id="password" placeholder="password" />
+        <br/><br/>
+        <input type="submit" id="login_btn" value="Login" />
+        <input type="submit" id="register_btn" value="Register" />
+        `;
+        document.getElementById('login_area').innerHTML = loginHtml;
     
-    var request = new XMLHttpRequest();
-
-	request.onreadystatechange = function () {
-
-		if(request.readyState === XMLHttpRequest.DONE){
-			if (request.status === 200) {
-				console.log('user logged in');
-			    location.reload();
-				alert('Logged in successfully');
-			}else if (request.status === 403){
-				alert('Username/Password is Invalid! Or Please Register.');
-				document.getElementById('username').value='';
-	            document.getElementById('password').value='';
-			}else if (request.status === 500){
-				alert('Something went wrong with our server.');
-			}
-
-		}
-
-	};
-
-	var username = document.getElementById('username').value;
-	var password = document.getElementById('password').value;
-
-	request.open('POST','http://dotarc.imad.hasura-app.io/login',true);   //dotarc.imad.hasura-app.io
-	request.setRequestHeader('Content-Type','application/json');
-	request.send(JSON.stringify({username:username,password:password}));
-};
-
-register.onclick = function(){
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        	if(request.readyState === XMLHttpRequest.DONE){
-			if (request.status === 200) {
-				console.log('Thank you for registering.');
-				alert('Thank you for registering.');
-				document.getElementById('username').value='';
-	            document.getElementById('password').value='';
-			}else if (request.status === 403){
-				alert('Registeration Unsuccessful.');
-				document.getElementById('username').value='';
-	            document.getElementById('password').value='';
-			}else if (request.status === 500){
-				alert('Something went wrong with our server.');
-			}
-
-		}
+    // Submit username/password to login
+    var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              loadLogin();
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+       if (username === '' || password === '') {
+        alert("Username/Password field can't be left empty");
+        return;
+    }
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
     };
-    var username = document.getElementById('username').value;
-	var password = document.getElementById('password').value;
+    
+    var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+      if (username === '' || password === '') {
+        alert("Username/Password field can't be left empty");
+        return;
+    }
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
+}
 
-	request.open('POST','http://dotarc.imad.hasura-app.io/create-user',true);   //dotarc.imad.hasura-app.io
-	request.setRequestHeader('Content-Type','application/json');
-	request.send(JSON.stringify({username:username,password:password}));
-};
+function loadLoggedInUser (username) {
+    var loginArea = document.getElementById('login_area');
+    loginArea.innerHTML = `
+        <h3> Hi <i>${username}</i></h3>
+        <a href="/logout">Logout</a>
+    `;
+}
 
-
-
-function checkLogin(){
+function loadLogin () {
+    // Check if the user is already logged in
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-                console.log(this.responseText);
                 loadLoggedInUser(this.responseText);
-               
+            } else {
+                loadLoginForm();
             }
         }
     };
+    
     request.open('GET', '/check-login', true);
     request.send(null);
 }
-function loadArticle() {
+function loadArticles () {
         // Check if the user is already logged in
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
-            var article = document.getElementById('articles');
+            var articles = document.getElementById('articles');
             if (request.status === 200) {
                 var content = '<ul>';
                 var articleData = JSON.parse(this.responseText);
                 for (var i=0; i< articleData.length; i++) {
                     content += `<li>
-                    <a href="/article/${articleData[i].title}">${articleData[i].heading}</a>
+                    <a href="/${articleData[i].title}">${articleData[i].heading}</a>
                     (${articleData[i].date.split('T')[0]})</li>`;
                 }
-                content += `</ul><div class="footer">
-                            <strong><a href="/post-article">Post an Artcile</a></strong>
-                            </div>`;
-                article.innerHTML = content;
+                content += "</ul>";
+                articles.innerHTML = content;
             } else {
-                article.innerHTML('Oops! Could not load all articles!');
+                articles.innerHTML('Oops! Could not load all articles!');
             }
         }
     };
@@ -107,18 +133,7 @@ function loadArticle() {
     request.open('GET', '/get-articles', true);
     request.send(null);
 }
-
-
-function loadLoggedInUser(username){
-    var logoutArea = document.getElementById('login_area');
-    var loginForm = document.getElementById('login-form');
-    loginForm.style.display = 'none';
-   
-    logoutArea.innerHTML = `
-        <h6> Hi <i>${username}</i></h6>
-        <a href="/logout">Logout</a>
-    `;
-}
-checkLogin();
+// The first thing to do is to check if the user is logged in!
+loadLogin();
+// Now this is something that we could have directly done on the server-side using templating too!
 loadArticles();
-
