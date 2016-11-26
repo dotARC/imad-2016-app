eacadef3f2eb05732d0a274d523756467b3a0a29
dotARC <mukeshbarca@gmail.com>
2016-11-26 12:13:00 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,54 +1,61 @@
-console.log('article.js');
-var currentArticleTitle = window.location.pathname.split('/')[2];
+var currentArticleTitle = window.location.pathname.split('/')[1];
 
-function loadCommentForm(){
-	var commentForm = `
-		<h4>Comments</h4>
-		<textarea id="comment_text" type="text" rows="5" cols="50" placeholder="Write your comment here.." id="comment"></textarea>
-		<br>
-		<input type="submit" id="submit" value="Submit" class="btn btn-success"/>`;
-
-		document.getElementById('comment_form').innerHTML = commentForm;
-
-		var submit = document.getElementById('submit');
-
-        submit.onclick = function(){
-		    
-			var request = new XMLHttpRequest();
-
-			request.onreadystatechange = function () {
-				if(request.readyState === XMLHttpRequest.DONE){
-					if (request.status === 200) {
-						document.getElementById('comment_text').value = '';
-						loadComments();
-					}else if(request.status === 403){
-						alert("Please write a comment.");
-					}else{
-					    	alert("Error! Could submit your comments.Please try later.");
-					}
-					submit.value='Submit';
-				}
-
-			};
-			var comment = document.getElementById('comment_text').value;
-
-			request.open('POST','/submit-comment/'+currentArticleTitle,true);   
-			request.setRequestHeader('Content-Type','application/json');
-			request.send(JSON.stringify({comment:comment}));
-			submit.value='Submitting..';
-
-		}
+function loadCommentForm () {
+    var commentFormHtml = `
+        <h5>Submit a comment</h5>
+        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
+        <br/>
+        <input type="submit" id="submit" value="Submit" />
+        <br/>
+        `;
+    document.getElementById('comment_form').innerHTML = commentFormHtml;
+    
+    // Submit username/password to login
+    var submit = document.getElementById('submit');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+                // Take some action
+                if (request.status === 200) {
+                    // clear the form & reload all the comments
+                    document.getElementById('comment_text').value = '';
+                    loadComments();    
+                } else {
+                    alert('Error! Could not submit comment');
+                }
+                submit.value = 'Submit';
+          }
+        };
+        
+        // Make the request
+        var comment = document.getElementById('comment_text').value;
+        if (comment ==='') {
+        alert("Comments field can't be left empty");
+        return;
+    }
+        request.open('POST', '/submit-comment/' + currentArticleTitle, true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({comment: comment}));  
+        submit.value = 'Submitting...';
+        
+    };
 }
 
-function checkLogin(){
+function loadLogin () {
+    // Check if the user is already logged in
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-                loadCommentForm(this.responseText);               
+                loadCommentForm(this.responseText);
             }
         }
     };
+    
     request.open('GET', '/check-login', true);
     request.send(null);
 }
@@ -61,34 +68,36 @@ function escapeHTML (text)
     return $div.innerHTML;
 }
 
-function loadComments(){
-	var request = new XMLHttpRequest();
-
-			request.onreadystatechange = function () {
-				var comments = document.getElementById('comments');
-				if(request.readyState === XMLHttpRequest.DONE){
-					if (request.status === 200) {
-						var content = '';
-						var commentsData = JSON.parse(this.responseText);
-						for(var i=0;i<commentsData.length;i++){
-							var time = new Date(commentsData[i].timestamp);
-							content+=`<div class="comment">
-								<p>${escapeHTML(commentsData[i].comment)}</p>
-								<div class="commenter">
-									${commentsData[i].username} - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()}
-								</div>
-								</div>`;
-						}
-						comments.innerHTML = content;
-					}else{
-						res.send('Oops! Could not load comments.');
-					}
-				}
-			};
-
-	request.open('GET', '/get-comments/'+currentArticleTitle, true);
-	request.send(null);
+function loadComments () {
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var comments = document.getElementById('comments');
+            if (request.status === 200) {
+                var content = '';
+                var commentsData = JSON.parse(this.responseText);
+                for (var i=0; i< commentsData.length; i++) {
+                    var time = new Date(commentsData[i].timestamp);
+                    content += `<div class="comment">
+                        <p>${escapeHTML(commentsData[i].comment)}</p>
+                        <div class="commenter">
+                            ${escapeHTML(commentsData[i].username)} - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()} 
+                        </div>
+                    </div>`;
+                }
+                comments.innerHTML = content;
+            } else {
+                comments.innerHTML('Oops! Could not load comments!');
+            }
+        }
+    };
+    
+    request.open('GET', '/get-comments/' + currentArticleTitle, true);
+    request.send(null);
 }
 
-checkLogin();
+
+// The first thing to do is to check if the user is logged in!
+loadLogin();
 loadComments();
