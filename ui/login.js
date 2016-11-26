d6671edf61d5f4a5c47ef5d9369c6ef2d1a4d71b
dotARC <mukeshbarca@gmail.com>
2016-11-26 23:44:05 +0530

    [imad-console] Updates ui/login.js

--- ui/login.js
+++ ui/login.js
@@ -118,9 +118,9 @@ function loadArticles () {
                 var content = '<ul>';
                 var articleData = JSON.parse(this.responseText);
                 for (var i=0; i< articleData.length; i++) {
-                    content += `<li>
+                    content += `
                     <a href="/article/${articleData[i].title}">${articleData[i].heading}</a>
-                    (${articleData[i].date.split('T')[0]})</li>`;
+                    (${articleData[i].date.split('T')[0]})`;
                 }
                 content += "</ul>";
                 articles.innerHTML = content;
